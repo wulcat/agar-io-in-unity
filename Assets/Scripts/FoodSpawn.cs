29c47ba60c07f9526536e0f130e112386cce216f
Zoe Love <zoelove.zle@gmail.com>
2016-05-31 21:26:37 +0530

    352q5

---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FoodSpawn : MonoBehaviour 
+{
+	//public GameObject _foodPrefab ;
+	public GameObject[] _foodPrefabArray ; 
+
+	public Vector2 Min ;
+	public Vector2 Max ;
+
+    
+    private Vector3 myFoodPos ;
+
+//	public Rigidbody2D foodClone ;
+
+	void Start() 
+	{
+		InvokeRepeating ("MyFoodSpawn", 5f, 0.5f);
+	}
+
+	void MyFoodSpawn() 
+	{
+		var i = Random.Range ( 0 ,  _foodPrefabArray.Length);
+
+		myFoodPos = new Vector3 ( Random.Range(Min.x , Max.x )  , Random.Range(Min.y , Max.y) , 0 )  ;
+        
+		var randomRandomClone = Instantiate (_foodPrefabArray [i], myFoodPos, transform.rotation) as Rigidbody2D;
+
+     //   var foodClone = Instantiate ( _foodPrefab, myFoodPos  , transform.rotation ) as Rigidbody2D ;
+
+	}
+}
---
+++ Assets/TrialScripts/Rendering.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PolygonCollider2D))]
+public class Rendering : MonoBehaviour {
+
+	public Vector3[] vertices ;
+	
+	public PolygonCollider2D _polyCollider ;
+	public Mesh _mesh ;
+	
+//	public int[] inPolygonPoints ;
+	
+	void Start()
+	{
+		_polyCollider = GetComponent<PolygonCollider2D>() ;
+		vertices = new Vector3[_polyCollider.points.Length] ;	
+//		vertices = _polyCollider.points ;
+		
+//		Debug.Log(_polyCollider.points.Length) ;
+
+		for (int i = 1 ; i < _polyCollider.points.Length ; i++)
+		{
+			vertices[i].x = _polyCollider.points[i].x ;
+			vertices[i].y = _polyCollider.points[i].y ;
+			
+	// make an array of int and keep adding every polyCollider Point reference to points vector lol made it messy to understand ..!		
+		}
+		
+		//Create new instance of mesh 
+		_mesh = new Mesh() ;
+		//Give a name to out mesh
+		_mesh.name = "SpriteRenderMesh" ;
+		_mesh.vertices = vertices ;
+		_mesh.uv = _polyCollider.points ;
+	//	_mesh.triangles =
+	
+		for(int i = 0 ; i < _polyCollider.points.Length ; i++)
+		{
+	//		_mesh.triangles[3 * i + 0] = _polyCollider.points[2] ;
+		}
+		
+		CreateTriangle() ;
+		
+		_mesh.RecalculateNormals() ;
+		GetComponent<MeshFilter>().mesh = _mesh ;
+		
+		Debug.Log("Total Surfaces : " +_polyCollider.shapeCount) ;
+		
+	}
+	
+	
+	void CreateTriangle()
+	{
+		
+		int m = 0 ;
+		int count = vertices.Length ;
+	
+		while ((count > 5))
+		{
+			count =  (count/2) ;
+			
+			if ((count > 5))
+			{
+				m++ ;
+			}
+			
+		}
+		Debug.Log("Total Polys Inside :"+m) ;
+		
+		
+		
+		
+		
+/*		for(int i = 0 ; inPolygonPoints[i] < 3 ; i++)
+		{
+	//		inPolygonPoints[] = new int[10] ;
+	//		inPolygonPoints[i] = new int[] { new (vertices.Length / 2) } ;
+		}
+*/	}
+	
+}
